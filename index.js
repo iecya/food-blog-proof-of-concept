403aa5051206e3b0186aa2697eccc283ac87beaf
federica <federica@strategic-blue.com>
2021-09-02 12:05:50 +0100

    get access to google drive api and list files

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const http = require('http');
+const {google} = require('googleapis');
 
 if (process.env.NODE_ENV != 'production') {
     require('dotenv').config();
 }
+
+const SCOPES = ["https://www.googleapis.com/auth/drive.readonly"]
+
+let oauth2Client = new google.auth.GoogleAuth({
+    keyFile: './private/service_account.json',
+    scopes: SCOPES
+});
+
+const drive = google.drive({
+    auth: oauth2Client,
+    version: 'v3'
+});
+
+const filesRes = drive.files.list();
+
+filesRes.then(val => console.log(val.data.files));
 
 const hostname = process.env.HOST;
 const port = process.env.PORT;
@@ -10,7 +27,9 @@ const port = process.env.PORT;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+    res.end(filesRes.then(val => {
+        val.data.files;
+    }));
   });
 
 
